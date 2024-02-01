a9f4b0ea098007d212ebb7c921e328498c107498
lucror1 <paulw2022@gmail.com>
2024-01-31 23:57:51 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -27,7 +28,13 @@ const budget = {
 app.use('/', express.static('public'));
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    // Based on https://stackoverflow.com/a/51655919
+    // Set the correct content type header for JSON
+    res.header("Content-Type", "application/json");
+
+    // Send the JSON file by concatenating the current directory with the file name to get the
+    // absolute path
+    res.sendFile(path.join(__dirname, "budget.json"));
 });
 
 app.listen(port, () => {
